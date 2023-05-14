4aa4f1d7de0027ede1e49ab9aca32433120295ef
Code-Nature <codenature01001@gmail.com>
2023-05-13 19:04:31 -0500

    Mostrando productos en ProductDetail

--- src/Components/ProductDetail/index.jsx
+++ src/Components/ProductDetail/index.jsx
@@ -5,6 +5,7 @@ import './style.css'
 
 export const ProductDetail = () => {
   const context = useContext(ShoppingCartContext)
+  console.log('PRODUCT TO SHOW: ', context.productToShow)
   return (
     <aside className={`${context.isProductDetailOpen ? 'flex' : 'hidden'} product-detail flex-col fixed right-0 border border-black rounded-lg bg-white`}>
       <div className='flex justify-between items-center p-6'>
@@ -13,6 +14,17 @@ export const ProductDetail = () => {
           <XMarkIcon className="h-6 w-6 text-black" />
         </div>
       </div>
+      <figure className='px-6'>
+        <img
+          className='w-full h-full rounded-lg'
+          src={context.productToShow.images[0]}
+          alt={context.productToShow.title} />
+      </figure>
+      <p className='flex flex-col p-6'>
+        <span className='font-medium text-2xl mb-2'>${context.productToShow.price}</span>
+        <span className='font-medium text-md'>{context.productToShow.title}</span>
+        <span className='font-medium text-sm'>{context.productToShow.description}</span>
+      </p>
     </aside>
   )
 }
